8e565a07707cba08366fe0e6875a9d95048fba13
Luís Serralheiro <encavadas@gmail.com>
2015-02-12 12:09:14 +0000

    updating the build

--- build/arenite.js
+++ build/arenite.js
@@ -443,6 +443,7 @@ Arenite.DI = function (arenite) {
         var imported = arenite.object.get(window, imp.namespace)();
         arenite.config = arenite.object.extend(arenite.config, imported);
         if (imported.imports) {
+          window.console.log('Arenite: Merging imports', imported.imports);
           imports = arenite.array.merge(imports, imported.imports);
         }
       }
@@ -477,6 +478,7 @@ Arenite.DI = function (arenite) {
     }
 
     if (arenite.config.imports) {
+      window.console.log('Arenite: Merging imports', arenite.config.imports);
       _mergeImports(JSON.parse(JSON.stringify(arenite.config.imports)));
     } else {
       _loadSyncDependencies();
