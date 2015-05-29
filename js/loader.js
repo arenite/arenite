82826eacfe5d34dd0e3cb5334bdcbf5d9dc021ff
Luís Serralheiro <encavadas@gmail.com>
2015-05-29 11:21:54 +0100

    improving loader for IE

--- js/loader.js
+++ js/loader.js
@@ -8,12 +8,15 @@ Arenite.Loader = function (arenite) {
     xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
     xhr.setRequestHeader("Access-Control-Allow-Origin", window.location.origin);
     if (arenite.config.withCredentials){
-    if ('withCredentials' in xhr) {
-      xhr.open(method, url, true);
-      xhr.withCredentials = true;
-    } else if (typeof XDomainRequest !== 'undefined') {
-      xhr = new XDomainRequest();
-      xhr.open(method, url);  
+      if ('withCredentials' in xhr) {
+        xhr.open(method, url, true);
+        xhr.withCredentials = true;
+      } else if (typeof XDomainRequest !== 'undefined') {
+        xhr = new XDomainRequest();
+        xhr.open(method, url);  
+      } else {
+        xhr=null
+      }
     } else {
       xhr.open(method, url, true);
     }
